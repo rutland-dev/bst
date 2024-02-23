0d3b9ef8f28fbc0d47e27dc2b2e5704e853925d0
Joshua Rutland <144169110+rutland-dev@users.noreply.github.com>
2024-02-23 10:04:58 -0500

    Set up directory structure with added files

--- webpack.common.js
+++ webpack.common.js
@@ -1,27 +1,30 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    index: './src/index.js',
+    index: "./src/index.js",
+    node: "./src/node.js",
+    tree: "./src/tree.js",
+    buildTree: "./src/build-tree.js",
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-      inject: 'body',
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "body",
     }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
